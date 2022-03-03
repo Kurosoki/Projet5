8e7660d656db5c3be44251af9325c6146ea92a6f
Kurosoki <diamanrosebis@gmail.com>
2022-03-03 15:05:38 +0100

    panier

---
+++ front/js/cart.js
@@ -0,0 +1,151 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+
+    //-------------------fonction principale-------------------//
+    //--------------------------------------------------------//
+    async function main() {
+
+
+        let ApiArray = [];
+
+        let localStorageArray = getLocalStorageProduct();
+
+        for (let i = 0; i < localStorageArray.length; i++) {
+            ApiArray.push(await GetApi(localStorageArray[i]));
+        }
+
+        let AllProducts = ConcatArray(localStorageArray, ApiArray);
+
+        DisplayProduct(AllProducts);
+
+        DisplayTotalPrice(AllProducts);
+
+        Listen(AllProducts);
+    }
+
+    main();
+
+    function getLocalStorageProduct() {
+
+        let getLocalStorageArray = [];
+
+        for (let i = 0; i < localStorage.length; i++) {
+            getLocalStorageArray[i] = JSON.parse(localStorage.getItem(localStorage.key(i)));
+        }
+
+        return getLocalStorageArray;
+    }
+
+    function GetApi(localStorageArray) {
+
+        return fetch("http://localhost:3000/api/products/" + localStorageArray.id)
+            .then(function (response) {
+                if (response.ok) {
+                    return response.json();
+                }
+
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    }
+
+    function ConcatArray(localStorageArray, ApiArray) {
+
+        let AllProducts = [];
+
+        for (let i = 0; i < localStorageArray.length; i++) {
+
+            let ObjectProduct = {
+                altTxt: ApiArray[i].altTxt,
+                colors: localStorageArray[i].color,
+                description: ApiArray[i].description,
+                imageUrl: ApiArray[i].imageUrl,
+                name: ApiArray[i].name,
+                price: ApiArray[i].price,
+                _id: localStorageArray[i].id,
+                qty: localStorageArray[i].qty
+            }
+
+            AllProducts.push(ObjectProduct);
+        }
+
+        return AllProducts;
+    }
+
+    function DisplayProduct(AllProducts) {
+
+        for (product of AllProducts) {
+
+            const domCreation = document.getElementById("cart__items");
+
+            domCreation.insertAdjacentHTML(
+                "beforeend",
+                `
+                <article class="cart__item" data-id="${product._id}" data-color="${product.colors}">
+                <div class="cart__item__img">
+                  <img src="${product.imageUrl}" alt="${product.altTxt}">
+                </div>
+                <div class="cart__item__content">
+                  <div class="cart__item__content__description">
+                    <h2>${product.name}</h2>
+                    <p>${product.colors}</p>
+                    <p>${product.price}</p>
+                  </div>
+                  <div class="cart__item__content__settings">
+                    <div class="cart__item__content__settings__quantity">
+                      <p>Qté : </p>
+                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.qty}">
+                    </div>
+                    <div class="cart__item__content__settings__delete">
+                      <p class="deleteItem">Supprimer</p>
+                    </div>
+                  </div>
+                </div>
+              </article>
+                `
+            );
+        }
+    }
+
+    function DisplayTotalPrice(AllProducts) {
+
+        let totalPrice = 0;
+        let totalQty = 0;
+
+        for (product of AllProducts) {
+            totalPrice += parseInt(product.qty * product.price);
+            totalQty += parseInt(product.qty);
+        }
+
+        const DtotalQty = document.getElementById("totalQuantity");
+        const DtotalPrice = document.getElementById("totalPrice");
+
+        DtotalPrice.innerText = totalPrice;
+        DtotalQty.innerText = totalQty;
+    }
+
+    function Listen(AllProducts) {
+        // Fonction si changement dans notre input quantity.
+        ecoutequantity(AllProducts);
+        // Fonction si on veux supprimer un éléments de la liste.
+        ecoutedeleteProduct(AllProducts);
+    }
+
+    function ecoutequantity(AllProducts) {
+
+
+    }
+
+    function ecoutedeleteProduct(AllProducts) {
+
+        let qtyinput = document.querySelectorAll(".itemQuantity");
+
+        qtyinput.forEach(function (input) {
+            input.addEventListener("change", function (inputevent) {
+                console.log(inputevent)
+            })
+        })
+
+
+    }
+});
--- front/js/product.js
+++ front/js/product.js
@@ -12,9 +12,13 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
         DisplayProduct(product);
 
+        BtnClick(product);
     }
 
     main();
+
+    //-------------------fonction recup l'ID du produit api-------------------//
+    //--------------------------------------------------------//
 
     function GetProduct(productId) {
         return fetch("http://localhost:3000/api/products/" + productId)
@@ -25,7 +29,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
                 console.log(error);
             });
     }
-
+    //-----fonction affiche un produit et ses détails dans la page produit------//
+    //--------------------------------------------------------//
     function DisplayProduct(product) {
         console.log(product)
         const title = document.getElementsByTagName("title")[0];
@@ -35,10 +40,88 @@ document.addEventListener("DOMContentLoaded", function (event) {
         const parentDescription = document.getElementById("description");
         const parentQuantity = document.getElementById("quantity");
 
+
         title.innerHTML = product.name;
         parentName.innerText = product.name;
         parentPrice.innerText = product.price;
+        parentDescription.innerText = product.description;
+        parentQuantity.innerText = product.quantity;
 
+
+        // Création de notre balise image avec les attributs.
+        const productImg = document.createElement("img");
+        productImg.setAttribute("src", product.imageUrl);
+        productImg.setAttribute("alt", product.altTxt);
+        // Push après notre balise à la fin de la liste.
+        parentImg[0].appendChild(productImg);
+
+        //* Création des choix couleur-------------------------------------------------
+        let SelecteurCouleur = document.getElementById("colors")
+        let options = product.colors
+        options.forEach(function (element) {
+            SelecteurCouleur.appendChild(new Option(element, element));
+        })
+
+    }
+
+    class ProductClass {
+        constructor(id, name, color, qty) {
+            this.id = id;
+            this.name = name;
+            this.color = color;
+            this.qty = qty;
+        }
+    }
+
+    function BtnClick(product) {
+
+        let colorChoosen = "";
+        let qtyChoosen = "";
+        let qty = "";
+        let BtnPanier = document.getElementById("addToCart")
+
+        let colorSelection = document.getElementById("colors")
+        colorSelection.addEventListener("change", function (e) {
+            colorChoosen = e.target.value;
+        })
+
+        let qtySelection = document.getElementById("quantity")
+        qtySelection.addEventListener("change", function (e) {
+            qty = e.target.value;
+        })
+
+        BtnPanier.addEventListener("click", function () {
+
+            let ProductLocalStorage = [];
+            let oldQty = 0;
+
+            for (let i = 0; i < localStorage.length; i++) {
+                ProductLocalStorage[i] = JSON.parse(localStorage.getItem(localStorage.key(i)));
+
+                if (product._id === ProductLocalStorage[i].id && colorChoosen === ProductLocalStorage[i].color) {
+                    oldQty = ProductLocalStorage[i].qty;
+                }
+
+            }
+
+            qtyChoosen = parseInt(oldQty) + parseInt(qty);
+
+            let productChoosen = new ProductClass(
+                product._id,
+                product.name,
+                colorChoosen,
+                qtyChoosen
+            );
+
+            if (colorChoosen != "" && qty >= 1 && qty <= 100) {
+                localStorage.setItem(
+                    product.name + " " + colorChoosen,
+                    JSON.stringify(productChoosen)
+                );
+            } else {
+                alert("tu t'est tromper ma caille");
+            }
+        });
     }
 
 });
